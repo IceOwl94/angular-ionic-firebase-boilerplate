af591995ae32414f2e47add63cb9f01d7e31f593
dezordithomas <dezordi.thomas@gmail.com>
2020-07-29 14:27:01 +0200

    remove env add env example

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA9kRUVmKYUQXUzGsKMu9NjbD78y8L19r4",
-    authDomain: "boilerplate-1b44f.firebaseapp.com",
-    databaseURL: "https://boilerplate-1b44f.firebaseio.com",
-    projectId: "boilerplate-1b44f",
-    storageBucket: "boilerplate-1b44f.appspot.com",
-    messagingSenderId: "1069222179026",
-    appId: "1:1069222179026:web:d1f6b482d6b183965ffd0b",
-    measurementId: "G-067675JPEW"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
